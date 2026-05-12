2e1d1a3023df620c58c53da34b9df9c98df0e20a
mokh88u-sudo <mokh88u@gmail.com>
2026-05-12 05:17:36 +0300

    Add files via upload

---
+++ lab_escape/project/config.php
@@ -0,0 +1,13 @@
+<?php
+$host = "localhost";
+$dbname = "lab_escape"; 
+$username = "root";
+$password = "";
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("فشل الاتصال بقاعدة البيانات: " . $e->getMessage());
+}
+?>
